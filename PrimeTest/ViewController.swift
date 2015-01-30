36a8e970ca52b6bc673d669176f760ff23156208
Mat Sletten <matsletten@reception-desk.bbbb.net>
2015-01-30 11:29:02 -0800

    updates

--- PrimeTest/ViewController.swift
+++ PrimeTest/ViewController.swift
@@ -9,7 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    
+    @IBOutlet weak var emptyTextField: UITextField!
+    @IBOutlet weak var emptyLabel: UILabel!
+    @IBAction func checkButtonPressed(sender: AnyObject)
+    {
+        var primeConfirmed = true
+        var numberGuess = emptyTextField.text.toInt()
+        
+        if (numberGuess != nil)
+        {
+            if (numberGuess < 1)
+            {
+                emptyLabel.text = "Please enter a positive number"
+            }
+            else
+            {
+                if (numberGuess == 1)
+                {
+                    emptyLabel.text = "1 is not a prime number"
+                }
+                else
+                {
+                    for var primeCheck = 2; primeCheck < numberGuess; ++primeCheck
+                    {
+                        if (numberGuess! % primeCheck == 0)
+                        {
+                            //number is not prime
+                            primeConfirmed = false
+                        }
+                    }
+                    if (primeConfirmed == true)
+                    {
+                        emptyLabel.text = "It is Prime!"
+                    }
+                    else
+                    {
+                        emptyLabel.text = "It is not prime"
+                    }
+                }
+                
+                
+            }
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
